4c36d9cc2fed07ac43bdedab8fba17bac7484f87
SrivatsavK <46974203+SrivatsavK@users.noreply.github.com>
2019-09-21 17:25:44 -0400

    Add files via upload

---
+++ agent.py
@@ -0,0 +1,53 @@
+import numpy as np
+from math import sqrt
+
+class Agent(object):
+
+    def __init__(self, csvParameters, dhor = 5, goalRadiusSq=1):
+        """ 
+            Takes an input line from the csv file,  
+            and initializes the agent
+        """
+        self.id = int(csvParameters[0]) # the id of the agent
+        self.gid = int(csvParameters[1]) # the group id of the agent
+        self.pos = np.array([float(csvParameters[2]), float(csvParameters[3])]) # the position of the agent 
+        self.vel = np.zeros(2) # the velocity of the agent
+        self.goal = np.array([float(csvParameters[4]), float(csvParameters[5])]) # the goal of the agent
+        self.prefspeed = float(csvParameters[6]) # the preferred speed of the agent
+        self.gvel = self.goal-self.pos # the goal velocity of the agent
+        self.gvel = self.gvel/(sqrt(self.gvel.dot(self.gvel )))*self.prefspeed       
+        self.maxspeed = float(csvParameters[7]) # the maximum sped of the agent
+        self.radius = float(csvParameters[8]) # the radius of the agent
+        self.goalRadiusSq =goalRadiusSq # parameter to determine if agent is close to the goal
+        self.atGoal = False # has the agent reached its goal?
+        self.dhor = dhor # the sensing radius
+
+
+    def computeNewVelocity(self, neighbors=[]):
+        """ 
+            Your code to compute the new velocity of the agent. 
+            You probably need to pass here a list of all the agents in the simulation to determine the agent's nearest neighbors
+        """       
+        if not self.atGoal:
+            self.vel= self.gvel   # here I just set the new velocity to be the goal velocity
+            
+            
+
+    def update(self, dt):
+        """ 
+            Code to update the velocity and position of the agents.  
+            as well as determine the new goal velocity 
+        """
+        if not self.atGoal:
+            self.pos += self.vel*dt   #update the position
+        
+            # compute the goal velocity for the next time step. do not modify this
+            self.gvel = self.goal - self.pos
+            distGoalSq = self.gvel.dot(self.gvel)
+            if distGoalSq < self.goalRadiusSq: 
+                self.atGoal = True  # goal has been reached
+            else: 
+                self.gvel = self.gvel/sqrt(distGoalSq)*self.prefspeed  
+            
+            
+  
